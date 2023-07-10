76d7c6c51cd356f8a52536043e339a8aa9d5f89e
yassine krika <yassinekrika2016@gmail.com>
2023-07-10 14:44:55 +0100

    code2

--- App.ts
+++ App.ts
@@ -1,19 +1,23 @@
-// logical solution
-export function maskify(cc: string): string {
-    let c: string = ''
-    if (cc.length >= 4) {
-      for (let i:number = 0; i < cc.length - 4; i++) {
-        c += '#'
-      }
-      c += cc.slice(-4)
-    } else {
-      return cc
-    }
-    return c
-}
-
-console.log('yassine')
-// one line solution
-// export function maskify(cc: string): string {
-//     return cc.length >= 4 ? '#'.repeat(cc.length - 4) + cc.slice(-4) : cc
+// export function reverseWords(str: string): string {
+//     return str.split(' ').map(word => word.split('').reverse().join('')).join(' ')
 // }
+
+
+// logical solution
+// export function maskify(cc: string): string {
+//     let c: string = ''
+//     if (cc.length >= 4) {
+//       for (let i:number = 0; i < cc.length - 4; i++) {
+//         c += '#'
+//       }
+//       c += cc.slice(-4)
+//     } else {
+//       return cc
+//     }
+//     return c
+// }
+
+// one line solution
+export function maskify(cc: string): string {
+    return cc.length >= 4 ? '#'.repeat(cc.length - 4) + cc.slice(-4) : cc
+}
