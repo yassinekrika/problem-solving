f267af295af270d7b01324ba4fe36d32e29184c6
yassine krika <yassinekrika2016@gmail.com>
2023-07-13 16:36:03 +0100

    test4

--- App.ts
+++ App.ts
@@ -34,8 +34,50 @@
 // -1050 -> -105
 // Zero alone is fine, don't worry about it. Poor guy anyway
 
-function noBoringZeros(n: number): number {
-    return n === 0 ? 0 : n % 10 !== 0 ? n : noBoringZeros(n / 10)
+// function noBoringZeros(n: number): number {
+//     return n === 0 ? 0 : n % 10 !== 0 ? n : noBoringZeros(n / 10)
+// }
+
+// console.log(noBoringZeros(0))
+
+// In this Kata, you should implement a function, that takes integer in range [0..255] (8 bits), and returns combination of pits and lands that encode the number. Result should have format of string: PLLPL... where P represents pit and L represents land. Combination should always start with pit. Numbers should be encoded in little-endian, which means you start from least significant bit.
+
+// Example
+// Input: 5
+
+// Binary representation (8 bits): 00000101
+
+// Output: PLLPPPPPP
+
+// Explanation:
+
+// Starts from P as per description
+// Changes to L because first bit is 1
+// Stays L because second bit is 0
+// Changes to P because third bit is 1
+// Stays P because fourth bit is 0
+// Stays P till the end because all subsequent bits are 0
+
+
+export function encodeCD(n: number): string {
+    let encode: string = 'P'
+    let nbr:string = '0'.repeat(8 - n.toString(2).length) + n.toString(2)
+    
+    let str:string = nbr.split('').reverse().map((n) => {
+      if (n === "1") {
+        if (encode[encode.length] === 'L'){
+          encode + 'P'
+        } else if (encode[encode.length] === 'P') {
+          encode + 'L'
+        }
+      } else {
+        encode + encode[encode.length]
+      }
+      return encode
+  //     return n === '1' && encode[encode.length - 1] === 'L' ? encode + 'P' : encode +'L'                                            
+    }).join('')
+    console.log(str)
+    return str
 }
 
-console.log(noBoringZeros(0))
+console.log(encodeCD(5))
